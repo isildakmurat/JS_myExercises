381cd1d4dff4b81157a15fb8b440cc2a4b93de01
isildakmurat <yazilimkursum210110@gmail.com>
2022-08-09 19:05:06 +0300

    nested loops

---
+++ nestedLoops.js
@@ -0,0 +1,23 @@
+//1 ila 100 arası random bir sayı üreten fonksiyon
+//-------------------------------------------------
+function sayiUret(){                            
+   return Math.ceil(Math.random() * 100)
+}
+
+
+//sayiUret fonksiyonunu 5 kere çalıştır
+//------------------------------------------------------------
+for(i=1; i<=5; i++){                            
+    console.log(sayiUret())
+}
+
+//5 kolon halinde sayiUret fonksiyonunu 5 kere çalıştır
+//-------------------------------------------------------------
+
+for(i=1; i<=5; i++){
+    console.log(i + '. KOLON')
+    for(j=1; j<=5; j++){
+        console.log(sayiUret())
+    }
+}
+
